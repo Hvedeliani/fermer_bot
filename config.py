3b22b4b1f835e0eec2f0d40c4023f9ea7b62ba10
Hvedeliani <hhvveedd@gmail.com>
2026-05-14 18:40:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-BOT_TOKEN = "8753990907:AAGWlD0ZDUIYiJeTMzc_uXm7-ln-ifmBhJA"
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8753990907:AAGWlD0ZDUIYiJeTMzc_uXm7-ln-ifmBhJA")
+
+# Ваш Telegram ID (замените на настоящий после того, как узнаете)
+ADMIN_ID = 233766218  # <<< СЮДА ВСТАВЬТЕ ВАШ ID
